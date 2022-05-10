0a0976bae4346951d43eaf4f78e01583aad382d5
Juan Manuel Figueroa <juanma@thirdlove.com>
2022-05-10 17:50:50 -0300

    Testing pytest github

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
+#import coverage
 from fastapi import FastAPI
 
 app = FastAPI()
+#scov = coverage.Coverage()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    a = 'coverage test'
+    b = 'coverage test v2'
+    return {"msg": "Hello World"}
+
+@app.get("/route")
+async def route():
+    m = 'more lines'
+    t = 'to coverage'
+    a = 'at all'
+    return {"msg": "route"}
+
+@app.get("/path")
+async def path():
+    m = 'more lines'
+    t = 'to coverage'
+    a = 'at all'
+    return {"msg": "path"}
+
+# @app.on_event("startup")
+# async def startup_event():
+#     cov.start()
+
+# @app.on_event("shutdown")
+# async def shutdown_event():
+#     cov.stop()
+#     cov.save()
+#     cov.html_report()
---
+++ test_main.py
@@ -0,0 +1,15 @@
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Hello World"}
+
+def test_invalid_route():
+    response = client.get("/invalid")
+    assert response.status_code == 404
